57e978796ebb20eb96a5c17cbe8c08240fec44e5
antoniozamora2002 <antonio.zamora@upeu.edu.pe>
2024-06-01 21:49:04 -0500

    prueba

--- class/ClientesConexion.php
+++ class/ClientesConexion.php
@@ -3,7 +3,7 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://panaderia.informaticapp.com/clientes',
+  CURLOPT_URL => 'https://panstock.informaticapp.com/clientes',
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => '',
   CURLOPT_MAXREDIRS => 10,
@@ -12,12 +12,12 @@ curl_setopt_array($curl, array(
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => 'GET',
   CURLOPT_HTTPHEADER => array(
-    'Authorization: Basic YTJhYTA3YWRmaGRmcmV4ZmhnZGZoZGZlcnR0Z2VJMzNOeC5PUUxobnU5eVBnbEJjQVJDMFgydnU5RUtxOm8yYW8wN29kZmhkZnJleGZoZ2RmaGRmZXJ0dGdleUY1TXJBdUh4eDZKYjdZR0VPMWE4UjFWYlFad2VnVw=='
+    'Authorization: Basic YTJhYTA3YWRmaGRmcmV4ZmhnZGZoZGZlcnR0Z2VCL3F6cjhOSS9yMS9QRi5XQmFnRGY5eXN5R21Wa0ptOm8yYW8wN29kZmhkZnJleGZoZ2RmaGRmZXJ0dGdlcHhoT0hsb2JKaVUvUi8ucnR1ZlJDWnpsOHhPZW4ucQ=='
   ),
 ));
 
 $response = curl_exec($curl);
 
 curl_close($curl);
-echo $response;
+$data =json_decode($response, true);
 ?>
--- view/clientes.php
+++ view/clientes.php
@@ -1,3 +1,9 @@
+<?php
+
+include '../class/ClientesConexion.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,35 +28,32 @@
             <tr class="bg-gray-200">
               <th class="px-4 py-2 text-left">#</th>
               <th class="px-4 py-2 text-left">Nombre & Apellido</th>
-              <th class="px-4 py-2 text-right">DNI</th>
-              <th class="px-4 py-2 text-right">Telefono</th>
-              <th class="px-4 py-2 text-right">Email</th>
+              <th class="px-4 py-2 text-center">DNI</th>
+              <th class="px-4 py-2 text-center">Telefono</th>
+              <th class="px-4 py-2 text-center">Email</th>
               <th class="px-4 py-2 text-center">Acciones</th>
             </tr>
           </thead>
           <tbody>
           <?php
-            $response = file_get_contents('../class/ClientesConexion.php');
-            $data = json_decode($response, true);
-            
-            if ($data && isset($data['Status']) && $data['Status'] == 200 && isset($data['Detalle'])) {
-                $clients = $data['Detalle'];
-                foreach ($clients as $client) {
-                    echo "<tr>
-                            <td class='border-b px-4 py-2'>{$client['cl_id']}</td>
-                            <td class='border-b px-4 py-2'>{$client['cl_nombre']} {$client['cl_apellido']}</td>
-                            <td class='border-b px-4 py-2 text-right'>{$client['cl_dni']}</td>
-                            <td class='border-b px-4 py-2 text-right'>{$client['cli_numero']}</td>
-                            <td class='border-b px-4 py-2 text-right'>{$client['cli_email']}</td>
-                            <td class='border-b px-4 py-2 text-center'>
-                                <button class='bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700 mr-2'>Editar</button>
-                                <button class='bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-700'>Eliminar</button>
-                            </td>
-                          </tr>";
-                }
-            } else {
-                echo "<tr><td colspan='6' class='text-center text-red-500'>No se encontraron clientes</td></tr>";
+          if ($data && isset($data['Status']) && $data['Status'] == 200 && isset($data['Detalle'])) {
+            $clients = $data['Detalle'];
+            foreach ($clients as $client) {
+                echo "<tr>
+                        <td class='border-b px-4 py-2'>{$client['cl_id']}</td>
+                        <td class='border-b px-4 py-2'>{$client['cl_nombre']} {$client['cl_apellido']}</td>
+                        <td class='border-b px-4 py-2 text-center'>{$client['cl_documento']}</td>
+                        <td class='border-b px-4 py-2 text-center'>{$client['cl_telefono']}</td>
+                        <td class='border-b px-4 py-2 text-center'>{$client['cl_email']}</td>
+                        <td class='border-b px-4 py-2 text-center'>
+                            <button class='bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700 mr-2'>Editar</button>
+                            <button class='bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-700'>Eliminar</button>
+                        </td>
+                      </tr>";
             }
+        } else {
+            echo "<tr><td colspan='6' class='text-center text-red-500'>No se encontraron clientes</td></tr>";
+        }
             ?>
           </tbody>
         </table>
